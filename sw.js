0aa391c758fc8d8a56827ba5a4d1c298f415658c
Alejo121097 <csv.libra.2013@gmail.com>
2025-07-26 23:13:40 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'rutina-postura-cache-v1';
+// Como tu app es un solo archivo, solo necesitamos cachear el archivo principal.
+const urlsToCache = [
+  './' 
+];
+
+// Evento de instalación: se abre el caché y se agregan los archivos.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Evento fetch: responde con los archivos del caché si están disponibles.
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Si el archivo está en caché, lo devuelve. Si no, lo busca en la red.
+        return response || fetch(event.request);
+      })
+  );
+});

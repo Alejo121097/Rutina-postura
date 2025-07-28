36f2f4f2ac864c706518a611ad5be178efe6ecfd
Alejo121097 <csv.libra.2013@gmail.com>
2025-07-27 23:24:18 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rutina-postura-cache-v5';
+const CACHE_NAME = 'rutina-postura-cache-v6';
 // Como tu app es un solo archivo, solo necesitamos cachear el archivo principal.
 const urlsToCache = [
   './' 
